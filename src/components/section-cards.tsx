d1a60d22f6d8aa0d4344d145dfc1d446f214dd5b
ahmoin <123082059+ahmoin@users.noreply.github.com>
2025-09-09 18:55:12 -0400

    feat: change section cards to have assignment related mock data

--- src/components/section-cards.tsx
+++ src/components/section-cards.tsx
@@ -15,9 +15,9 @@ export function SectionCards() {
 		<div className="*:data-[slot=card]:from-primary/5 *:data-[slot=card]:to-card dark:*:data-[slot=card]:bg-card grid grid-cols-1 gap-4 px-4 *:data-[slot=card]:bg-gradient-to-t *:data-[slot=card]:shadow-xs lg:px-6 @xl/main:grid-cols-2 @5xl/main:grid-cols-4">
 			<Card className="@container/card">
 				<CardHeader>
-					<CardDescription>Total Revenue</CardDescription>
+					<CardDescription>Unweighted GPA</CardDescription>
 					<CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-						$1,250.00
+						3.98
 					</CardTitle>
 					<CardAction>
 						<Badge variant="outline">
@@ -30,16 +30,16 @@ export function SectionCards() {
 					<div className="line-clamp-1 flex gap-2 font-medium">
 						Trending up this month <IconTrendingUp className="size-4" />
 					</div>
-					<div className="text-muted-foreground">
-						Visitors for the last 6 months
-					</div>
+					{/* <div className="text-muted-foreground">
+						Your GPA since ...
+					</div> */}
 				</CardFooter>
 			</Card>
 			<Card className="@container/card">
 				<CardHeader>
-					<CardDescription>New Customers</CardDescription>
+					<CardDescription>Late Submitted Assignments</CardDescription>
 					<CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-						1,234
+						2
 					</CardTitle>
 					<CardAction>
 						<Badge variant="outline">
@@ -50,31 +50,29 @@ export function SectionCards() {
 				</CardHeader>
 				<CardFooter className="flex-col items-start gap-1.5 text-sm">
 					<div className="line-clamp-1 flex gap-2 font-medium">
-						Down 20% this period <IconTrendingDown className="size-4" />
+						Down 20% this marking period <IconTrendingDown className="size-4" />
 					</div>
-					<div className="text-muted-foreground">
-						Acquisition needs attention
-					</div>
+					<div className="text-muted-foreground">Significant progress made</div>
 				</CardFooter>
 			</Card>
 			<Card className="@container/card">
 				<CardHeader>
-					<CardDescription>Active Accounts</CardDescription>
+					<CardDescription>Average Assignment Grade</CardDescription>
 					<CardTitle className="text-2xl font-semibold tabular-nums @[250px]/card:text-3xl">
-						45,678
+						98.231
 					</CardTitle>
 					<CardAction>
 						<Badge variant="outline">
 							<IconTrendingUp />
-							+12.5%
+							+2.5%
 						</Badge>
 					</CardAction>
 				</CardHeader>
 				<CardFooter className="flex-col items-start gap-1.5 text-sm">
 					<div className="line-clamp-1 flex gap-2 font-medium">
-						Strong user retention <IconTrendingUp className="size-4" />
+						Very good consistency <IconTrendingUp className="size-4" />
 					</div>
-					<div className="text-muted-foreground">Engagement exceed targets</div>
+					<div className="text-muted-foreground">Exceeding target</div>
 				</CardFooter>
 			</Card>
 			<Card className="@container/card">
